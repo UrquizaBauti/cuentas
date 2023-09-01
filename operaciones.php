d61881c7d5bddc256ba492af89d45c844fef4aa0
UrquizaBauti <42178581@terciariourquiza.edu.ar>
2023-08-31 22:48:37 -0300

    operaciones php

---
+++ operaciones.php
@@ -0,0 +1,21 @@
+<?php
+$n1 = $_GET['n1'];
+$n2 = $_GET['n2'];
+$operacion = $_GET['operacion'];
+
+if($operacion== "suma")
+{
+    $total = $n1 + $n2;
+}
+elseif($operacion == "resta")
+{
+    $total = $n1 - $n2;
+}
+elseif($opreacion == "multiplicacion")
+{
+    $total = $n1 * $n2;
+}
+else if($operacion == "division")
+{
+    $total = $n1 / $n2;
+}
